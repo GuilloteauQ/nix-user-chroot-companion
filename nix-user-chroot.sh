ecbfeb030e3a90a5ea9f4ad5993e2bc94fdc16fc
Quentin Guilloteau <Quentin.Guilloteau@inria.fr>
2021-11-26 14:37:15 +0100

    nix develop

--- nix-user-chroot.sh
+++ nix-user-chroot.sh
@@ -34,5 +34,6 @@ fi
 
 # clear
 echo "Activate Nix"
-nix_user_chroot_bash="$nix_user_chroot_cmd $nix_store_dir bash --rcfile $nix_user_chroot_dir/nix_setenv.sh -ci 'cd $1; nix-shell -p poetry --run \"poetry install && poetry run nxc build -f $2 -l -o $3\"'"
+# nix_user_chroot_bash="$nix_user_chroot_cmd $nix_store_dir bash --rcfile $nix_user_chroot_dir/nix_setenv.sh -ci 'cd $1; nix-shell -p poetry --run \"poetry install && poetry run nxc build -f $2 -l -o $3\"'"
+nix_user_chroot_bash="$nix_user_chroot_cmd $nix_store_dir bash --rcfile $nix_user_chroot_dir/nix_setenv.sh -ci 'cd $1; nix develop --command nxc build -f $2 -o $3'"
 eval $nix_user_chroot_bash
