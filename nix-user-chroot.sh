aee1720cd6a20496cd0a236761843d54154e6860
Quentin Guilloteau <Quentin.Guilloteau@grenoble-inp.org>
2022-03-04 17:28:30 +0100

    fix cli

--- nix-user-chroot.sh
+++ nix-user-chroot.sh
@@ -39,6 +39,7 @@ if [ "$1" == "store" ]; then
     nix_user_chroot_bash="$nix_user_chroot_cmd $nix_store_dir bash --rcfile $nix_user_chroot_dir/nix_setenv.sh -ci 'nix-store -qR $2 > $3'"
     eval $nix_user_chroot_bash
 else
-    nix_user_chroot_bash="$nix_user_chroot_cmd $nix_store_dir bash --rcfile $nix_user_chroot_dir/nix_setenv.sh -ci 'cd $1; nix develop --command nxc build -C '$2' -o $3'"
+    # nxc build --composition-flavour 'hello::g5k-ramdisk' --flavour g5k-ramdisk -o plop
+    nix_user_chroot_bash="$nix_user_chroot_cmd $nix_store_dir bash --rcfile $nix_user_chroot_dir/nix_setenv.sh -ci 'cd $1; nix develop --command nxc build --composition-flavour ${2}::${3} --flavour ${3} -o ${4}'"
     eval $nix_user_chroot_bash
 fi
